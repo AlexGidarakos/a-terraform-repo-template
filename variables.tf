4c91d869670c5e6ba4c9b82ea9ee285a38582a95
Alexandros Gidarakos <algida79@gmail.com>
2023-04-02 20:29:49 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+# Start section: Base variables
+variable "project" {
+  type        = string
+  description = "Name of the project"
+}
+
+variable "owner" {
+  type        = string
+  description = "Name of the person or team that owns the project"
+}
+# End section: Base variables
