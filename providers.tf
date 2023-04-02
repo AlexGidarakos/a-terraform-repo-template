12cf8c56db39ae48315bf65988dba7d5d0ed5b98
Alexandros Gidarakos <algida79@gmail.com>
2023-04-02 20:34:16 +0100

    Add some placeholder sections

--- providers.tf
+++ providers.tf
@@ -1,6 +1,18 @@
 terraform {
   required_version = ">= x.y.z"
 
+  backend "backend_name" {
+  }
+
   required_providers {
+    provider_name = {
+      source  = "hashicorp/provider_name"
+      version = "~> x.y.z"
+    }
+  }
+
+  provider "provider_name" {
+    features {
+    }
   }
 }
